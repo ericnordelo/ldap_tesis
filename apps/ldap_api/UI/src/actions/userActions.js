f73ab5c1db09c87c53806316280c62f3e137e555
Eric Nordelo <eric.nordelo39@gmail.com>
2019-08-15 13:08:25 -0400

    Modifying user actions

--- apps/ldap_api/UI/src/actions/userActions.js
+++ apps/ldap_api/UI/src/actions/userActions.js
@@ -42,35 +42,35 @@ function addWorker(data) {
 }
 
 function addStudent(data) {
-  return function(dispatch) {
-    dispatch({ 
-        type: types.START_LOADING
-    });
-    fetch(config.api_address + '/p/preguntasdeseguridad', {
-        credentials: "include",
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-    })
-    .then(res => {
-      if (res.status === 200) {
-          dispatch(push('/entrar'));
-          alert('Su contraseña ha sido actualizada satisfactoriamente.');
-      } else{
-          alert('Las respuestas no son las adecuadas.');
-      }
-      dispatch({ 
-          type: types.END_LOADING
-      });
-    })
-    .catch(err => {
-        console.error(err);
-        alert('Las respuestas no son las adecuadas.');
+    return function(dispatch) {
         dispatch({ 
-          type: types.END_LOADING
-      });
-    });
-  };
+            type: types.START_LOADING
+        });
+        fetch(config.api_address + '/trabajadores', {
+            credentials: "include",
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            if (res.status === 200) {
+                dispatch(push('/p/cambiar'));
+                alert('Estudiante agregado satisfactoriamente con el correo: por favor cambie su contraseña.');
+            } else{
+                alert('Sus credenciales son incorrectas.');
+            }
+            dispatch({ 
+                type: types.END_LOADING
+            });
+        })
+        .catch(err => {
+            dispatch({ 
+                type: types.END_LOADING
+            });
+            console.error(err);
+            alert('Ha ocurrido un error mientras se agregaba la cuenta.');
+        });
+    };
 }
